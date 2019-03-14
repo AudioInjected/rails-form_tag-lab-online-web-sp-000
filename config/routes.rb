5ad5c147e8dec57f5446dc032b9b739f18d944fa
Rafael Batista <rafaelb24@yahoo.com>
2019-03-14 01:54:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  resource :student, only: :create
+  resources :students, only: [:index, :create]
+
   get '/students/:id', to: 'students#show', as: 'student'
 end
